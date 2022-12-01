2df86c62fc2cc1436180797cab0733040187a4ff
jsilevine <jsilevine@berkeley.edu>
2022-12-01 17:42:34 -0500

    "add plotting methods for water-only sim"

--- simulator/water_only/main.jl
+++ simulator/water_only/main.jl
@@ -1,3 +1,4 @@
+using Base: NonReshapedReinterpretArray
 ##---------------------------------------------------------------
 ## WATER_ONLY_SIMULATOR -- main.jl
 ##
@@ -11,7 +12,7 @@
 
 using Plots, QuadGK, DataFrames, Distributions,
     SpecialFunctions, LazySets, NLsolve, AutoPreallocation,
-    Profile, PProf
+    Profile, PProf, PlotThemes
 
 ## simulation parameters
 Nspp::Int64 = 10; Nyr::Int64 = 1000; Ninit::Float64 = 1.0;
@@ -30,8 +31,6 @@ include("simulation_functions.jl")
 include("eq_functions.jl")
 include("meta_functions.jl")
 
-
-
 ##---------------------------------------------------------------
 ## 1. check agreement between simulator and equilibrium calculations
 ##---------------------------------------------------------------
@@ -46,77 +45,62 @@ plot_eq_agreement(out, true, "figures/sim_vs_an.pdf")
 ## 2. calculate equilibrium densities under constant, initial
 ##    water contents and interrain interval lengths
 ##---------------------------------------------------------------
+@time results = multi_eq_constant_water(30,8, 0.1, 0.6, 6, 1.0, 100.0, 6);
 
-function dens_est_constant_water(Nspp::Int64 = 10, Niter::Int64 = 10,
-                                 minW₀::Float64 = 0.1, maxW₀::Float64 = 0.6, lengthW₀::Int64 = 10,
-                                 minT::Float64 = 1.0, maxT::Float64 = 100.0, lengthT::Int64 = 10)
 
-    W₀_list = collect(range(minW₀, stop = maxW₀, length = lengthW₀));
-    T_list = collect(range(minT, stop = maxT, length = lengthT));
-    params = collect(Base.product(W₀_list, T_list));
+theme(:dark)
+my_cgrad = cgrad(:acton)
 
-    spp_data = generate_spp_data(Nspp, 0.6) ## initialize spp_data
-    full_results = Array{Float64}(undef, Nspp*Niter, length(params))
-    Threads.@threads for i in 1:length(params)
-        sub_results = Vector{Float64}(undef, Nspp*Niter)
-        for j in 1:Niter
-            spp_data = generate_spp_data(Nspp, 0.8, params[i][2], 100.0, 0.1, 2.5, 0.4, 0.0)
-            sub_results[[((j-1)*Nspp)+1:1:j*Nspp;]] = calc_eqN(spp_data, F, E, params[i][1])[:,:eqN]
-        end
-        full_results[:,i] = sub_results
+
+x = :W₀
+typeof(x)
+
+typeof("x")
+
+function plot_multi_eq(data::DataFrame, yvar::String = "n", xvar::Symbol = :W₀)
+
+    if xvar == :W₀
+        groupvar = :T
+    else
+        groupvar = :W₀
     end
 
-    return [full_results, Niter, params]
+    subdata = data[data.var .== yvar, :]
+    p = plot(subdata[:,xvar], subdata.mean, group = subdata[:,groupvar], line_z = subdata[:,groupvar],
+             fill_z = subdata[:,groupvar], ribbon = subdata.sd, seriescolor = my_cgrad,
+             seriestype = :line, ylim = [0, Nspp], xlim = [minimum(subdata[:,xvar]), maximum(subdata[:,xvar])],
+             legend = :topleft, frame = :box, grid = false, linewidth = 3, fillalpha = 0.3)
+    return p
 
-end
+end;
 
-@time results = dens_est_constant_water(30,8);
+plot_multi_eq(summary)
 
-nfeas = Vector{Float64}(undef, (size(results[1])[2]))
-for i in 1:size(results[1])[2]
-    nfeas[i] = count(results[1][:,i] .> 0.0) / results[2]
-
-end
-
-plot([results[3][i][j] for i = 1:100, j = 1:2][:,1], nfeas,
-     group = [results[3][i][j] for i = 1:100, j = 1:2][:,2], seriestype = :scatter, ylim = [0,30], legend = :bottomright)
-
-plot([results[3][i][j] for i = 1:100, j = 1:2][:,2], nfeas,
-     group = [results[3][i][j] for i = 1:100, j = 1:2][:,1], seriestype = :scatter, ylim = [0,30], legend = :bottomright)
-
-plot(results[1][:,12], seriestype = :histogram)
+plot(subdata.T, subdata.mean, group = subdata.W₀, line_z = subdata.W₀, fill_z = subdata.W₀,
+     ribbon = subdata.sd, seriescolor = my_cgrad,
+     seriestype = :line, ylim = [0, Nspp], xlim = [minimum(subdata.T), maximum(subdata.T)],
+     legend = :topleft, frame = :box, grid = false, linewidth = 3, fillalpha = 0.3)
 
 
+subdata = summary[summary.var .== "avg", :]
+plot(subdata.W₀, subdata.mean, group = subdata.T, line_z = subdata.T, fill_z = subdata.T,
+     ribbon = subdata.sd, seriescolor = my_cgrad,
+     seriestype = :line, ylim = [0, Nspp], xlim = [minimum(subdata.W₀), maximum(subdata.W₀)],
+     legend = :topleft, frame = :box, grid = false, linewidth = 3, fillalpha = 0.3)
+
+plot(subdata.T, subdata.mean, group = subdata.W₀, line_z = subdata.W₀, fill_z = subdata.W₀,
+     ribbon = subdata.sd, seriescolor = my_cgrad,
+     seriestype = :line, ylim = [0, Nspp], xlim = [minimum(subdata.T), maximum(subdata.T)],
+     legend = :topleft, frame = :box, grid = false, linewidth = 3, fillalpha = 0.3)
 
 
+subdata = summary[summary.var .== "min", :]
+plot(subdata.W₀, subdata.mean, group = subdata.T, line_z = subdata.T, fill_z = subdata.T,
+     ribbon = subdata.sd, seriescolor = my_cgrad,
+     seriestype = :line, ylim = [0, Nspp], xlim = [minimum(subdata.W₀), maximum(subdata.W₀)],
+     legend = :topleft, frame = :box, grid = false, linewidth = 3, fillalpha = 0.3)
 
-
-
-
-
-
-
-
-spp_data = generate_spp_data(Nspp, 0.8, 56.0, 100.0, 0.1, 2.5, 0.4, 0.0)
-calc_eqN(spp_data, F, E, 0.1)
-
-
-
-spp_data = generate_spp_data(8, 0.6)
-Profile.Allocs.@profile out = sim_water_only(spp_data, 1000, 8, Ninit);
-PProf.Allocs.pprof(from_c = false)
-
-biomass_dynamics = out[1]
-n_dynamics = out[2]
-n_data = out[3]
-check_eq(biomass_dynamics, 1e-7)
-calc_eqN(spp_data, F)
-Vector(out[3][nrow(out[3]),:])
-
-plot(vcat(0,spp_data.tau), vcat(W0, spp_data.Wi), seriestypes = :scatter)
-
-plot(stack(biomass_dynamics).rowkey, stack(biomass_dynamics).value.+1, group = stack(biomass_dynamics).variable,
-     linewidth = 3, grid = false, framestyle = :box, xlims = (0,Nyr), yscale = :log10)
-
-plot(stack(n_dynamics).rowkey, stack(n_dynamics).value, group = stack(n_dynamics).variable,
-     linewidth = 3, grid = false, framestyle = :box, yscale = :log10, xlims = (0, Nyr))
+plot(subdata.T, subdata.mean, group = subdata.W₀, line_z = subdata.W₀, fill_z = subdata.W₀,
+     ribbon = subdata.sd, seriescolor = my_cgrad,
+     seriestype = :line, ylim = [0, Nspp], xlim = [minimum(subdata.T), maximum(subdata.T)],
+     legend = :topleft, frame = :box, grid = false, linewidth = 3, fillalpha = 0.3)
--- simulator/water_only/meta_functions.jl
+++ simulator/water_only/meta_functions.jl
@@ -5,6 +5,63 @@
 ## November 2022
 ##---------------------------------------------------------------
 
-function ()
+"""
+    dens_est_constant_water(Nspp::Int64 = 10, Niter::Int64 = 10,
+                                 minW₀::Float64 = 0.1, maxW₀::Float64 = 0.6, lengthW₀::Int64 = 10,
+                                 minT::Float64 = 1.0, maxT::Float64 = 100.0, lengthT::Int64 = 10)
+
+TBW
+"""
+function multi_eq_constant_water(Nspp::Int64 = 10, Niter::Int64 = 10,
+                                 minW₀::Float64 = 0.1, maxW₀::Float64 = 0.6, lengthW₀::Int64 = 10,
+                                 minT::Float64 = 1.0, maxT::Float64 = 100.0, lengthT::Int64 = 10)
+
+    W₀_list = collect(range(minW₀, stop = maxW₀, length = lengthW₀));
+    T_list = collect(range(minT, stop = maxT, length = lengthT));
+    params = collect(Base.product(W₀_list, T_list));
+
+    spp_data = generate_spp_data(Nspp, 0.6) ## initialize spp_data
+    full_results = Array{Float64}(undef, Nspp*Niter, length(params))
+    Threads.@threads for i in 1:length(params)
+        sub_results = Vector{Float64}(undef, Nspp*Niter)
+        for j in 1:Niter
+            spp_data = generate_spp_data(Nspp, 0.8, params[i][2], 100.0, 0.1, 2.5, 0.4, 0.0)
+            sub_results[[((j-1)*Nspp)+1:1:j*Nspp;]] = calc_eqN(spp_data, F, E, params[i][1])[:,:eqN]
+        end
+        full_results[:,i] = sub_results
+    end
+
+    return [full_results, Niter, params, Nspp]
+
+end
+
+function summarize_multi_eq(multo_eq_output)
+
+    ## initialize data
+    Niter = multi_eq_output[2]; Nspp = multi_eq_output[4]; tmp = Vector{Int64}; Npar = size(multi_eq_output[1])[2];
+    summary = DataFrame(W₀ = repeat([multi_eq_output[3][i][j] for i = 1:Npar, j = 1:2][:,1], outer = 4),
+                        T = repeat([multi_eq_output[3][i][j] for i = 1:Npar, j = 1:2][:,2], outer = 4),
+                        var = repeat(["n", "min", "max", "avg"], inner = Npar),
+                        mean = Vector{Float64}(undef, Npar*4),
+                        sd = Vector{Float64}(undef, Npar*4))
+
+    nfeas_temp = Vector{Float64}(undef, Niter); maxfeas_temp = Vector{Float64}(undef, Niter);
+    minfeas_temp = Vector{Float64}(undef, Niter); avgfeas_temp = Vector{Float64}(undef, Niter);
+
+    for i in 1:size(multi_eq_output[1])[2]
+        for j in 1:Niter
+            tmp = findall(multi_eq_output[1][[(Nspp*(j-1))+1:1:Nspp*j;],i] .> 0.0)
+            maxfeas_temp[j] = maximum(tmp, init = 0)
+            minfeas_temp[j] = minimum(tmp, init = 31)
+            avgfeas_temp[j] = mean(tmp)
+            nfeas_temp[j] = length(tmp)
+        end
+        summary[i, :mean] = mean(nfeas_temp); summary[i, :sd] = std(nfeas_temp)
+        summary[Npar+i, :mean] = mean(minfeas_temp); summary[Npar+i, :sd] = std(minfeas_temp)
+        summary[Npar*2+i, :mean] = mean(maxfeas_temp); summary[Npar*2+i, :sd] = std(maxfeas_temp)
+        summary[Npar*3+i, :mean] = mean(avgfeas_temp); summary[Npar*3+i, :sd] = std(avgfeas_temp)
+    end
+
+    return summary
 
 end;
